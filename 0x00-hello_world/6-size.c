ee6b5544f10e9a8393531d3c90a5ec50a9d4bf30
safaa-suliman <safomohmedahmed@gmail.com>
2023-08-13 07:51:01 -0700

    Edit more than 80 char error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,6 @@ printf("Size of an int: %zu byte(s)\n", (unsigned long)sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(long int));
 printf("Size of a long long int: %zu", (unsigned long)sizeof(long long int));
 printf(" byte(s)\n");
-printf("Size of a float: %zu byte(s)\n", (unsigned long) sizeof(float));
+printf("Size of a float: %zu byte(s)", (unsigned long) sizeof(float));
 return (0);
 }
