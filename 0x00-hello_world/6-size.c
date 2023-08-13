b4712a2736ee5dbc51757f20f4c2d674cf5da1b6
safaa-suliman <safomohmedahmed@gmail.com>
2023-08-13 07:41:50 -0700

    Edit more than 80 char error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,11 @@
  */
 int main(void)
 {
-printf("Size of a char:  %lu byte(s)\n", (unsigned long)sizeof(char));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
-printf("Size of a float: %lu byte(s)", (unsigned long) sizeof(float));
+printf("Size of a char:  %zu byte(s)\n", (unsigned long)sizeof(char));
+printf("Size of an int: %zu byte(s)\n", (unsigned long)sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(long int));
+printf("Size of a long long int: %zu", (unsigned long)sizeof(long long int));
+printf("byte(s)\n");
+printf("Size of a float: %zu byte(s)", (unsigned long) sizeof(float));
 return (0);
 }
