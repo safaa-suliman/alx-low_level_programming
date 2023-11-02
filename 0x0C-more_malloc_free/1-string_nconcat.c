a30f9f1af195192aacd0651ef20a2fcfc28f27f0
safa-suliman <safomohmedahmed@gmail.com>
2023-11-02 14:25:42 -0700

    malloc, free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,42 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 /**
- * _strdup - duplicate to new memory space location
- * @str: char
- * Return: 0
- */
-char *_strdup(char *str)
+* string_nconcat - a function that concatenates two strings
+* @s1: first char
+* @s2: secound char
+* @n: unsigned int
+* Return: If the function fails, it should return NULL
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-
-	char *aaa;
-
-	int i, r = 0;
-
-	if (str == NULL)
-		return (NULL);
-
-	i = 0;
-
-	while (str[i] != '\0')
-
-		i++;
+unsigned int x, y, z;
+char *s;
 
 
-	aaa = malloc(sizeof(char) * (i + 1));
-
-
-	if (aaa == NULL)
-
-		return (NULL);
-
-
-	for (r = 0; str[r]; r++)
-
-		aaa[r] = str[r];
-
-
-
-	return (aaa);
-
+if (s1 == NULL)
+{
+x = 0;
+}
+else
+{
+for (x = 0; s1[x]; ++x)
+;
+}
+if (s2 == NULL)
+{
+y = 0;
+}
+else
+{
+for (y = 0; s2[y]; ++y)
+;
+}
+if (y > n)
+y = n;
+s = malloc(sizeof(char) * (x + y + 1));
+if (s == NULL)
+return (NULL);
+for (z = 0; z < x; z++)
+s[z] = s1[z];
+for (z = 0; z < y; z++)
+s[z + x] = s2[z];
+s[x + y] = '\0';
+return (s);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,13 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *create_array(unsigned int size, char c);
+
+void _print(char *str, int l);
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+int check_for_digits(char **av);
+void init(char *str, int l);
+#include <stdio.h>
+#include <stdlib.h>
 int _putchar(char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 
 #endif
